bb324a2baf1c9d8781b845f87685ffc2c3849fa7
Anees K <akodappana@korewireless.com>
2021-07-22 20:14:09 +0530

    first commit

---
+++ src/components/Skeleton.js
@@ -0,0 +1,9 @@
+function Skeleton({
+  circle = false,
+  width = 100,
+  height = 100,
+  background = "#eaeaea",
+  align = "left",
+}) {
+  return <div></div>;
+}
---
+++ src/components/TextSkeleton.js
@@ -0,0 +1,3 @@
+function TextSkeleton({ lines = 1, width = 100, height = 40 }) {
+  return <div></div>;
+}
